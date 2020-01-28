809afc05f7ee6d8e94162b5af81bad295eaaafa2
Kevin Schoell <schoell411@gmail.com>
2020-01-28 16:47:31 -0600

    added tests from group zoom

--- star-wars-app/src/components/StarWarsCharacters.js
+++ star-wars-app/src/components/StarWarsCharacters.js
@@ -5,59 +5,66 @@ import { getData } from "../api";
 import "./star-wars-characters.css";
 
 export default function StarWarsCharacters() {
-  const [url, setUrl] = useState("https://swapi.co/api/people");
-  const [previous, setPrevious] = useState();
-  const [next, setNext] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [characters, setCharacters] = useState([]);
-  useEffect(() => {
-    setIsLoading(true);
-    const getCharacters = async () => {
-      const characters = await getData(url);
-      console.log(characters);
-      setNext(characters.next);
-      setPrevious(characters.previous);
-      setCharacters(characters.results);
-      setIsLoading(false);
-    };
-    getCharacters();
-  }, [url]);
+	const [url, setUrl] = useState("https://swapi.co/api/people");
+	const [previous, setPrevious] = useState();
+	const [next, setNext] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+	const [characters, setCharacters] = useState([]);
 
-  const goToNext = e => {
-    e.preventDefault();
-    setUrl(next);
-  };
+	// added for testing
+	const [message, setMessage] = useState("");
+	const fakeApiCall = () => Promise.resolve("Success");
 
-  const goToPrevious = e => {
-    e.preventDefault();
-    setUrl(previous);
-  };
+	useEffect(() => {
+		setIsLoading(true);
+		const getCharacters = async () => {
+			const characters = await getData(url);
+			console.log(characters);
+			setNext(characters.next);
+			setPrevious(characters.previous);
+			setCharacters(characters.results);
+			setIsLoading(false);
+		};
+		getCharacters();
+	}, [url]);
 
-  return (
-    <div>
-      {isLoading ? (
-        <Loader
-          type="ThreeDots"
-          color="#FFC402"
-          height={30}
-          width={100}
-          timeout={3000} //3 secs
-        />
-      ) : (
-        <>
-          {characters.map(character => (
-            <div key={character.url}>{character.name}</div>
-          ))}
-        </>
-      )}
-      <div className="buttons">
-        <button onClick={goToPrevious} disabled={!previous}>
-          Previous
-        </button>
-        <button onClick={goToNext} disabled={!next}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
+	const goToNext = e => {
+		e.preventDefault();
+		setUrl(next);
+	};
+
+	const goToPrevious = e => {
+		e.preventDefault();
+		setUrl(previous);
+	};
+
+	return (
+		<div>
+			{isLoading ? (
+				<Loader
+					type="ThreeDots"
+					color="#FFC402"
+					height={30}
+					width={100}
+					timeout={3000} //3 secs
+				/>
+			) : (
+				<>
+					{characters.map(character => (
+						<div key={character.url}>{character.name}</div>
+					))}
+				</>
+			)}
+			<div className="buttons">
+				<button onClick={goToPrevious} disabled={!previous}>
+					Previous
+				</button>
+				<button onClick={goToNext} disabled={!next}>
+					Next
+				</button>
+
+				<span>{message}</span>
+			</div>
+		</div>
+	);
 }
--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -2,43 +2,55 @@ import React from "react";
 import { render, fireEvent, wait, findByText } from "@testing-library/react";
 import { getData as mockGetData } from "../api";
 import StarWarsCharacters from "./StarWarsCharacters";
+import App from "../App";
 import { act } from "react-dom/test-utils";
 
+// AAA Arrange, Act, Assert <- the flow for writing tests
+
 jest.mock("../api");
+
+test("render App", () => {
+	render(<App />);
+});
+
+test("renders previous/next buttons", () => {
+	const { getByText } = render(<StarWarsCharacters />);
+	const previousButton = getByText(/previous/i);
+	const nextButton = getByText(/next/i);
+
+	act(() => {
+		fireEvent.click(previousButton, nextButton);
+		// fireEvent.click(nextButton);
+	});
+	// act(() => {
+	// });
+	findByText(/success/i);
+});
 
 const fakeData = {
 	next: "https://swapi.co/api/people/?page=2",
 	previous: null,
 	results: [
-		{ name: "Boba", url: "1" },
+		{ name: "Bob", url: "1" },
 		{ name: "2", url: "2" }
 	]
 };
 
 mockGetData.mockResolvedValue(fakeData);
 
-test("renders previous/next buttons", () => {
-	// AAA Arrange, Act, Assert
-	const { getByText } = render(<StarWarsCharacters />);
-	const previousButton = getByText(/Previous/i);
-	const nextButton = getByText(/Next/i);
-
-	act(() => {
-		fireEvent.click(previousButton);
-		fireEvent.click(nextButton);
-	});
-	findByText(/success/i);
-});
-
-// test('renders success test', () => {
-// 	const { queryByText } = render(<StarWarsCharacters />);
-// })
-
 test("api test", async () => {
 	const { getByText } = render(<StarWarsCharacters />);
 
-	await wait(() => expect(getByText(/boba/i)));
-	getByText(/Boba/i);
+	await wait(() => expect(getByText(/bob/i)));
+	getByText(/Bob/i);
 
 	expect(mockGetData).toHaveBeenCalledWith("https://swapi.co/api/people");
+});
+
+test("renders success text from fakeApiCall", () => {
+	const { getByText, findByText } = render(<App />);
+	act(() => {
+		fireEvent.click(getByText("Next"));
+	});
+	findByText(/Success/i);
 });
